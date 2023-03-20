adfa72dcbd4416a850b87eabe620978320a98495
Matias Psara <matiaspsara@gmail.com>
2023-03-20 21:32:02 +0000

    Inicio del TP, archivo de interfaz grafica

---
+++ JuegoGrillaVentana.java
@@ -0,0 +1,63 @@
+package trabajoPractico1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import javax.swing.table.DefaultTableModel;
+
+public class JuegoGrillaVentana {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JuegoGrillaVentana window = new JuegoGrillaVentana();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JuegoGrillaVentana() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Juegos aritmeticos");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		table = new JTable();
+		table.setRowHeight(20);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		frame.getContentPane().add(table, BorderLayout.CENTER);
+	}
+
+}
