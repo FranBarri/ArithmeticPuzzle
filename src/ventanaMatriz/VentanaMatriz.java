c42085e5f6161c87932de4ed6775c4dbcc5c2253
Francisco Barrientos <ftbarrientos103@gmail.com>
2023-03-24 19:31:16 -0300

    primer commit

---
+++ src/ventanaMatriz/VentanaMatriz.java
@@ -0,0 +1,76 @@
+package ventanaMatriz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+
+public class VentanaMatriz {
+
+	private JFrame frame;
+	private JScrollPane scrollPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaMatriz window = new VentanaMatriz();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaMatriz() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblMatriz = new JLabel("MATRIZ");
+		lblMatriz.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMatriz.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblMatriz.setBounds(0, 0, 434, 32);
+		frame.getContentPane().add(lblMatriz);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(45, 55, 344, 195);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		scrollPane.setViewportView(table);
+	}
+}
